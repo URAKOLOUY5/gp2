ceefb324394d0c23d35b3cfe987547f9b4a3800e
URAKOLOUY5 <URAKOLOUY2@yandex.ru>
2024-09-26 17:14:10 +0300

    Fadeout in point_changelevel

--- lua/entities/point_changelevel.lua
+++ lua/entities/point_changelevel.lua
@@ -5,11 +5,19 @@
 
 ENT.Type = "point"
 
+local black = Color(0,0,0)
+
 function ENT:AcceptInput(name, activator, caller, data)
     name = name:lower()
 
-    if name == "changelevel" then
-        RunConsoleCommand("changelevel", data)     
+    if name == "changelevel" then    
+        for _, ply in ipairs(player.GetAll()) do
+            ply:ScreenFade(SCREENFADE.OUT, black, 0.25, 0.5)
+        end
+
+        timer.Simple(0.25, function()
+            RunConsoleCommand("changelevel", data)
+        end)
     elseif name == "changelevelpostfade" then
         RunConsoleCommand("changelevel", data)
     end
