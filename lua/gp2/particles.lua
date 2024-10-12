93a1f33632ba3a1d572a944404ee686bce8c87d0
URAKOLOUY5 <URAKOLOUY2@yandex.ru>
2024-10-12 19:37:05 +0300

    Update particles.lua

--- lua/gp2/particles.lua
+++ lua/gp2/particles.lua
@@ -3,7 +3,7 @@
 -- Particles precaching and etc.
 -- ----------------------------------------------------------------------------
 
-for _, file in ipairs(file.Find("data_static/particles/*.pcf", "GAME")) do
+for _, file in ipairs(file.Find("particles/*.pcf", "GAME")) do
     print('Adding file: ' .. file)
-    game.AddParticles("data_static/particles/" .. file)
+    game.AddParticles("particles/" .. file)
 end
